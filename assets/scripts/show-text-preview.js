69487d4d33c551cd00227d1e45c91d4eae1d61f2
Jason Garvin <wearesilence@gmail.com>
2023-09-05 09:04:58 -0600

    Fix errors in blog toggle display
    Adjust javascript logic to remove errors at runtime caused by targeting
    hidden elements prematurely. Swapped the if statement to fix the issue.

--- assets/scripts/show-text-preview.js
+++ assets/scripts/show-text-preview.js
@@ -1,20 +1,19 @@
 // Used to manage Read more / Read less options on blog posts
 
 function toggleLongPreview(elementID, btnID) {
-    const previewFrame = document.getElementById(elementID);
-    const activeButton = document.getElementById(btnID);
+  const previewFrame = document.getElementById(elementID);
+  const activeButton = document.getElementById(btnID);
 
-    let dots = previewFrame.getElementsByClassName("dots");
-    let moreText = previewFrame.getElementsByClassName("read-more");
+  let dots = previewFrame.getElementsByClassName("dots");
+  let moreText = previewFrame.getElementsByClassName("read-more");
 
-    if (dots[0].hidden === false) {
-      dots[0].hidden = true;
-      moreText[0].hidden = false;
-      activeButton.innerHTML = "Read less";
-    } else {
-      dots[0].hidden = false;
-      moreText[0].hidden = true;
-      activeButton.innerHTML = "Read more";
-
-    }
+  if (dots[0].hidden === true) {
+    dots[0].hidden = false;
+    moreText[0].hidden = true;
+    activeButton.innerHTML = "Read more";
+  } else {
+    dots[0].hidden = true;
+    moreText[0].hidden = false;
+    activeButton.innerHTML = "Read less";
   }
+}
