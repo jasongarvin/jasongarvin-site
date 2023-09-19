8188399134727135b994088a412dd63e7716bba0
Jason Garvin <66984922+Unfayr@users.noreply.github.com>
2023-09-18 21:21:39 -0600

    Theme toggle now changes fa icon
    Use javascript in toggle-theme.js to dynamically change from a sun icon
    to a moon icon within the toggle element to reflect the current theme status.

--- assets/scripts/toggle-theme.js
+++ assets/scripts/toggle-theme.js
@@ -3,6 +3,7 @@
 
 // Get the theme toggle button element
 const themeToggle = document.getElementById('toggle-theme');
+const themeIcon = document.getElementById('theme-icon');
 
 // Check if the user's theme preference is already set
 const userThemePreference = localStorage.getItem('theme');
@@ -10,12 +11,18 @@ const userThemePreference = localStorage.getItem('theme');
 // If the preference is set, apply the theme
 if (userThemePreference === 'dark') {
   document.body.classList.add('dark-theme');
+  themeIcon.className = 'fa fa-moon-o';
 }
 
 // Event listener for the theme toggle button
 themeToggle.addEventListener('click', () => {
   // Toggle the dark theme class on the body element
   document.body.classList.toggle('dark-theme');
+  if (themeIcon.className === 'fa fa-sun-o') {
+    themeIcon.className = 'fa fa-moon-o';
+  } else {
+    themeIcon.className = 'fa fa-sun-o';
+  }
 
   // Save the user's theme preference to Local Storage
   const currentTheme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
