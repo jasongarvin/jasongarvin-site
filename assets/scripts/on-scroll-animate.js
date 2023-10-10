10cb22f9e4ea2938ae605c7e53eb0a1600109d64
Jason Garvin <wearesilence@gmail.com>
2023-10-10 08:35:46 -0600

    Add scroll animations to home page
    Create and apply drop-in scroll animations for social links below
    both Total Escape Games and Fill and Fire. Finish JavaScript scaffolding
    to prepare for further home page redesigns

--- assets/scripts/on-scroll-animate.js
+++ assets/scripts/on-scroll-animate.js
@@ -1,12 +1,21 @@
 // Sets certain elements to animate when they intersect with the viewport
 
-const pageviewWipeObserver = new IntersectionObserver(entries => {
+const animatedElements = document.querySelectorAll('.animate-drop');
+for (const element of animatedElements) {
+    element.style.opacity = '0';
+}
+
+const pageviewObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
+            console.log('intersected')
             entry.target.classList.add('drop-into-frame');
+            entry.target.style.opacity = '1';
         }
     });
+}, {
+    rootMargin: '0px 0px -30% 0px'
 });
 
-// Tell the observer which elements to track
-pageviewWipeObserver.observe(document.querySelector('.animate-drop'));
+// Tell the observer to track all elements with the class `.animate-drop`
+animatedElements.forEach(element => pageviewObserver.observe(element));
