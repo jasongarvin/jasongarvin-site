72439936aa1ad3c6581ea07d54b213ccb86bb998
Jason Garvin <66984922+Unfayr@users.noreply.github.com>
2023-10-11 15:19:00 -0600

    Get new contact form working with formspree
    Add formspree endpoint, javascript to handle form submission

---
+++ assets/scripts/contact-form-submit.js
@@ -0,0 +1,40 @@
+// A quick script providing submit confirmation
+
+const contactForm = document.getElementById('contact-me');
+
+function resetButton(buttonId, newContent) {
+  buttonId.textContent = newContent;
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  let submitButton = document.getElementById('contact-submit');
+  let formContent = new FormData(event.target);
+
+  fetch(event.target.action, {
+    method: contactForm.method,
+    body: formContent,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+        submitButton.textContent = 'Thanks for your submission!';
+        contactForm.reset()
+        setTimeout(resetButton, 1000, submitButton, 'Submit');
+    } else {
+      response.json().then(formContent => {
+        if (Object.hasOwn(formContent, 'errors')) {
+          submitButton.textContent = formContent['errors'].map(error => error['message']).join(', ')
+        } else {
+          submitButton.textContent = 'Oops! There was a problem submitting your form'
+        }
+      })
+    }
+  }).catch(error => {
+    submitButton.textContent = 'Oops! There was a problem submitting your form';
+  });
+}
+
+contactForm.addEventListener('submit', handleSubmit)
