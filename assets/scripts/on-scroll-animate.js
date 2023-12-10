6bcfde8d99a8c8c1060ab0ad1307e1ad1603c7aa
Jason Garvin <66984922+Unfayr@users.noreply.github.com>
2023-12-10 14:25:23 -0700

    Fix back-to-top button, header javascript
    Adjust JS code to use onscroll for only the header, and use a different
    method of checking when to add the back-to-top button by evaluating page intersection
    instead of using the onScroll() function

--- assets/scripts/on-scroll-animate.js
+++ assets/scripts/on-scroll-animate.js
@@ -2,22 +2,22 @@
 
 const animatedElements = document.querySelectorAll('.animate-drop');
 for (const element of animatedElements) {
-    element.style.opacity = '0';
+  element.style.opacity = '0';
 }
 
 const pageviewObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            console.log('intersected')
-            entry.target.classList.add('drop-into-frame');
-            entry.target.style.opacity = '1';
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      console.log('intersected')
+      entry.target.classList.add('drop-into-frame');
+      entry.target.style.opacity = '1';
 
-            pageviewObserver.unobserve(entry.target);
-        }
-    });
+      pageviewObserver.unobserve(entry.target);
+    }
+  });
 }, {
-    rootMargin: '0px 0px -30% 0px'
+  rootMargin: '0px 0px -30% 0px'
 });
 
 // Tell the observer to track all elements with the class `.animate-drop`
