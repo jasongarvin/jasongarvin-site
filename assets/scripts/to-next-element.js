630ab4539b70f619a7cad7091421152472f21440
Jason Garvin <66984922+Unfayr@users.noreply.github.com>
2023-12-28 10:34:50 -0700

    Refactor old JavaScript
    Refine code in to-next-element.js such that calculations are only done inside the function body when necessary, and initial DOM calculations are done outside the function.

--- assets/scripts/to-next-element.js
+++ assets/scripts/to-next-element.js
@@ -1,12 +1,10 @@
 // Manage scroll effects between tagged sections
 
-// TODO: Revise script to dynamically add classes to buttons using js
-// Get the button
-// let buttonIndexes = document.getElementsByClassName("scroll-to");
+// Get the elements we want to scroll to
+const sectionIndexes = document.getElementsByClassName("js-next-section");
 
 // When the user clicks on the button, scroll to the next section
 function toNextSection(buttonNumber) {
-  let sectionIndexes = document.getElementsByClassName("js-next-section");
   let nextSection = sectionIndexes[buttonNumber];
   let scrollY = nextSection.offsetTop;
 
