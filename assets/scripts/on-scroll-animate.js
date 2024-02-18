d2888e41e67af112e832542230862a281ea8865f
Jason Garvin <wearesilence@gmail.com>
2024-02-17 19:37:39 -0700

    Add animations / styles to home page
    Create new animation styles for the home page, including slide-in effects, and apply them throughout the home page.
    
    Refine button styles both for the CTA buttons and for default buttons throughout the site.

--- assets/scripts/on-scroll-animate.js
+++ assets/scripts/on-scroll-animate.js
@@ -1,5 +1,6 @@
 // Set elements with animate classes to animate when they intersect the viewport
 
+// Drop-in animation
 const animatedElements = document.querySelectorAll('.animate-drop');
 for (const element of animatedElements) {
   element.style.opacity = '0';
@@ -17,8 +18,30 @@ const pageviewObserver = new IntersectionObserver(entries => {
     }
   });
 }, {
-  rootMargin: '0px 0px -30% 0px'
+  rootMargin: '0px 0px -20% 0px'
 });
 
-// Tell the observer to track all elements with the class `.animate-drop`
+// Slide-in animation
+const slidingElements = document.querySelectorAll('.animate-slide');
+for (const element of slidingElements) {
+  element.style.opacity = '0'
+}
+
+const pageviewObserver2 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      console.log('intersected')
+      entry.target.classList.add('slide-into-frame');
+      entry.target.style.opacity = '1';
+
+      pageviewObserver.unobserve(entry.target);
+    }
+  });
+}, {
+  rootMargin: '0px 0px -10% 0px'
+});
+
+// Tell the observer to track all elements with animate classes
 animatedElements.forEach(element => pageviewObserver.observe(element));
+slidingElements.forEach(element => pageviewObserver2.observe(element));
